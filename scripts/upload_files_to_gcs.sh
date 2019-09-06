504d4d0068dafc699fece9d5cdd08492ca763d6c
Artyom Palvelev <cppg@cppg>
2019-09-06 17:19:07 +0000

    fixed paths

--- scripts/upload_files_to_gcs.sh
+++ scripts/upload_files_to_gcs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gsutil -m cp train_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
-gsutil -m cp val_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
-gsutil -m cp val_human_parts.json gs://ap_tpu_storage/converted/human_parts/
+gsutil -m cp output/train_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
+gsutil -m cp output/val_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
+gsutil -m cp output/val_human_parts.json gs://ap_tpu_storage/converted/human_parts/
