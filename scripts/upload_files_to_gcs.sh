f36a28ae60c20b037b5fea94fdb0085bee20ef99
artyom <cppg@cppg.co.uk>
2019-09-06 18:16:51 +0100

    updated the upload script to upload proper tfrecords

--- scripts/upload_files_to_gcs.sh
+++ scripts/upload_files_to_gcs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-gsutil -m cp val* gs://ap_tpu_storage/converted/
-
+gsutil -m cp train_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
+gsutil -m cp val_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
+gsutil -m cp val_human_parts.json gs://ap_tpu_storage/converted/human_parts/
