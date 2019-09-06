579d06747b396465c62f6111c557b2905a30b7a3
Artyom Palvelev <cppg@cppg>
2019-09-06 17:38:41 +0000

    fixed train script

--- scripts/train_human_parts.sh
+++ scripts/train_human_parts.sh
@@ -13,12 +13,13 @@ export PART=human_parts
 export MODEL_DIR=${STORAGE_BUCKET}/saved/1.0.3-$PART
 export TRAIN_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/train*
 export EVAL_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/val*
-export VAL_JSON_FILE=${STORAGE_BUCKET}/val_$PART.json
+export VAL_JSON_FILE=${STORAGE_BUCKET}/converted/$PART/val_$PART.json
 
 
-tensorboard --logdir $MODEL_DIR &
+# tensorboard --logdir $MODEL_DIR &
 
-python models/official/detection/main.py --use_tpu=True --tpu="${TPU_NAME?}" \
+python ../models/official/detection/main.py --use_tpu=True --tpu="${TPU_NAME?}" \
     --num_cores=8 --model_dir="${MODEL_DIR?}" --mode="train_and_eval" \
     --params_override="{ type: retinanet, train: { checkpoint: { path: ${RESNET_CHECKPOINT?}, prefix: resnet50/ }, train_file_pattern: ${TRAIN_FILE_PATTERN?} }, eval: { val_json_file: ${VAL_JSON_FILE?}, eval_file_pattern: ${EVAL_FILE_PATTERN?}, eval_samples: ${EVAL_SAMPLES?}, num_steps_per_eval: ${NUM_STEPS_PER_EVAL?} } }" \
-    --config_file models/official/detection/configs/yaml/1.0.3_constant.yaml
+    --config_file ../models/official/detection/configs/yaml/1.0.3_constant.yaml
+
