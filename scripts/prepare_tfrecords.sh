ac2b45e45a4ab17515ad2853675ab2a5915650c1
artyom <cppg@cppg.co.uk>
2019-09-06 17:38:00 +0100

    fixed tfrecords build script

--- scripts/prepare_tfrecords.sh
+++ scripts/prepare_tfrecords.sh
@@ -3,12 +3,14 @@
 PYTHON=${PYTHON:-python3}
 PYTHONPATH=$PYTHONPATH:$HOME/dev/frameworks/tensorflow_models:$HOME/dev/frameworks/tensorflow_models/research
 
-# python3.6 gen_tfrecords.py \
-#     --image_dir data/validation/ \
-#     --output_prefix output/val_human_parts \
-#     --image_info_file output/val_human_parts.csv \
-#     --classes_file output/classes_human_parts.csv \
-#     --num_shards=10
+set -e
+
+$PYTHON gen_tfrecords.py \
+    --image_dir data/validation/ \
+    --output_prefix output/val_human_parts \
+    --image_info_file output/val_human_parts.csv \
+    --classes_file output/classes_human_parts.csv \
+    --num_shards=10
 
 $PYTHON gen_tfrecords.py \
     --image_dir data/train/ \
