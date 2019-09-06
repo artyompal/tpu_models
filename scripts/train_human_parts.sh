a1b369167d93c37324c548bbee388290458ade3d
artyom <cppg@cppg.co.uk>
2019-09-06 18:33:33 +0100

    fixed tensorboard params

--- scripts/train_human_parts.sh
+++ scripts/train_human_parts.sh
@@ -16,9 +16,9 @@ export EVAL_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/val*
 export VAL_JSON_FILE=${STORAGE_BUCKET}/val_$PART.json
 
 
+tensorboard --logdir $MODEL_DIR &
+
 python models/official/detection/main.py --use_tpu=True --tpu="${TPU_NAME?}" \
     --num_cores=8 --model_dir="${MODEL_DIR?}" --mode="train_and_eval" \
     --params_override="{ type: retinanet, train: { checkpoint: { path: ${RESNET_CHECKPOINT?}, prefix: resnet50/ }, train_file_pattern: ${TRAIN_FILE_PATTERN?} }, eval: { val_json_file: ${VAL_JSON_FILE?}, eval_file_pattern: ${EVAL_FILE_PATTERN?}, eval_samples: ${EVAL_SAMPLES?}, num_steps_per_eval: ${NUM_STEPS_PER_EVAL?} } }" \
     --config_file models/official/detection/configs/yaml/1.0.3_constant.yaml
-
-tensorboard --logdir gs://ap_tpu_storage/saved/1.0.0
