19062bd8949cc313fc7ddfc6e8e0314a154fe345
artyom <cppg@cppg.co.uk>
2019-09-07 13:55:02 +0100

    updated the upload script

--- scripts/upload_files_to_gcs.sh
+++ scripts/upload_files_to_gcs.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
 gsutil -m cp output/train_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
 gsutil -m cp output/val_human_parts*.tfrecord gs://ap_tpu_storage/converted/human_parts/
 gsutil -m cp output/val_human_parts.json gs://ap_tpu_storage/converted/human_parts/
+
+for i in {0..4}
+do
+    echo "========================================================================"
+    echo "processing part $i"
+
+    PART="part_$i"
+
+    gsutil -m cp output/train_$PART*.tfrecord gs://ap_tpu_storage/converted/$PART/
+    gsutil -m cp output/val_$PART*.tfrecord gs://ap_tpu_storage/converted/$PART/
+    gsutil -m cp output/val_$PART.json gs://ap_tpu_storage/converted/$PART/
+done
