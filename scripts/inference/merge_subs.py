cb2317e9d86d93ff93b7c2086edb1d84d0ff958e
artyom <cppg@cppg.co.uk>
2019-09-11 18:56:42 +0100

    added a script to combine predictions from different parts of the dataset

---
+++ scripts/inference/merge_subs.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3.6
+
+import argparse
+import itertools
+import sys
+
+from typing import List
+
+import pandas as pd
+
+from tqdm import tqdm
+
+
+def merge(lines: List[str]) -> str:
+    preds = []
+    step = 6
+
+    for line in lines:
+        values = line.split()
+
+        for ofs in range(0, len(values), step):
+            preds.append(values[ofs : ofs + step])
+            assert len(preds[-1]) == step
+
+            # print(preds[-1])
+            # if ofs > 6:
+            #     break
+
+    preds.sort(key=lambda pred: -float(pred[1]))
+    res = ' '.join(itertools.chain(*preds))
+
+    # print(res)
+    # sys.exit()
+
+    return res
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('result', help='result filename', type=str)
+    parser.add_argument('filenames', help='submissions from partial datasets', nargs='*')
+    args = parser.parse_args()
+    assert len(args.filenames) > 1
+
+    print('reading predictions')
+    predicts = [pd.read_csv(fname) for fname in tqdm(args.filenames)]
+    result = predicts[0].copy()
+
+    print('combining predictions')
+    for line in tqdm(range(result.shape[0])):
+        result.PredictionString[line] = merge([pred.PredictionString[line] for pred in predicts])
+
+    result.to_csv(args.result, index=False)
