a160acb964f92c055810cabb55cdb59f9644d05f
artyom <cppg@cppg.co.uk>
2019-09-14 10:12:55 +0100

    submissions merge script: added threshold, added filename guess

--- scripts/inference/merge_subs.py
+++ scripts/inference/merge_subs.py
@@ -2,6 +2,7 @@
 
 import argparse
 import itertools
+import os
 import sys
 
 from typing import List
@@ -22,16 +23,22 @@ def merge(lines: List[str]) -> str:
             preds.append(values[ofs : ofs + step])
             assert len(preds[-1]) == step
 
+    preds = list(filter(lambda pred: float(pred[1]) > args.min_conf, preds))
     preds.sort(key=lambda pred: -float(pred[1]))
     res = ' '.join(itertools.chain(*preds))
     return res
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
+    parser.add_argument('--min_conf', help='confidence threshold', type=float, default=0.02)
     parser.add_argument('result', help='result filename', type=str)
     parser.add_argument('filenames', help='submissions from partial datasets', nargs='*')
     args = parser.parse_args()
     assert len(args.filenames) > 1
+
+    if os.path.exists(args.result):
+        print(args.result, 'already exists, exiting')
+        sys.exit()
 
     print('reading predictions')
     predicts = [pd.read_csv(fname) for fname in tqdm(args.filenames)]
