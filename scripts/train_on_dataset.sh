54628004ef96479788eb2c0d4dca4b1eb6b85178
artyom <cppg@cppg.co.uk>
2019-09-21 17:15:14 +0100

    fixed training script

--- scripts/train_on_dataset.sh
+++ scripts/train_on_dataset.sh
@@ -16,16 +16,16 @@ PYTHONPATH=$HOME/tpu_models/models
 STORAGE_BUCKET=gs://ap_tpu_storage
 
 MODEL_DIR=${STORAGE_BUCKET}/saved/$VERSION-$PART
-TRAIN_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/train_${PART}_*.tfrecord
+TRAIN_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/train_${PART}*.tfrecord
 EVAL_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/val*.tfrecord
 VAL_JSON_FILE=${STORAGE_BUCKET}/converted/$PART/validation_$PART.json
 
-gsutil -q stat $TRAIN_FILE_PATTERN
-if (( $? ))
-then
-    echo 'balanced train dataset is not found, using the normal dataset'
-    TRAIN_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/train*.tfrecord
-fi
+# gsutil -q stat $TRAIN_FILE_PATTERN
+# if (( $? ))
+# then
+#     echo 'balanced train dataset is not found, using the normal dataset'
+#     TRAIN_FILE_PATTERN=${STORAGE_BUCKET}/converted/$PART/train*.tfrecord
+# fi
 
 mkdir -p output
 gsutil cp $VAL_JSON_FILE output/
@@ -67,4 +67,3 @@ python ../models/official/detection/main.py --use_tpu=$USE_TPU --tpu=$TPU_NAME \
     }" \
     --config_file `find ../models/official/detection/configs/yaml/ -name $VERSION*.yaml` \
     2>&1 | tee -a ~/$VERSION-$PART.log
-
