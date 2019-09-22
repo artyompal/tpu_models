0e9f0bdea67a0ed51ecbd489a93f54411b63b2dc
artyom <artyom@artyom>
2019-09-22 19:25:37 +0100

    simplified inference procedure

--- scripts/inference/inference.py
+++ scripts/inference/inference.py
@@ -7,6 +7,7 @@ import pickle
 import tensorflow as tf
 import pandas as pd
 
+from glob import glob
 from tqdm import tqdm
 
 
@@ -15,8 +16,16 @@ if __name__ == '__main__':
     parser.add_argument('directory', help='TF SavedModel directory', type=str)
     args = parser.parse_args()
 
+    model_dir = args.directory[args.directory.find('best_models'):]
+    model_dir = glob(model_dir + '/*')[0]
+    dirs = os.path.normpath(model_dir).split('/')
+    assert len(dirs) > 1
+    dest_filename = dirs[-2] + '.pkl'
+
+    print('model_dir', model_dir, 'dest_filename', dest_filename)
+
     tf.enable_eager_execution()
-    model = tf.saved_model.load_v2(args.directory, ['serve'])
+    model = tf.saved_model.load_v2(model_dir, ['serve'])
 
     signature = model.signatures["serving_default"]
     print('supported outputs:', signature.structured_outputs)
@@ -26,13 +35,12 @@ if __name__ == '__main__':
 
     predictions = []
     for image_id in tqdm(image_ids):
-        filename = f'data/test/{image_id}.jpg'
+        img_filename = f'data/test/{image_id}.jpg'
 
-        with open(filename, 'rb') as f:
+        with open(img_filename, 'rb') as f:
             data = f.read()
             pred = signature(tf.constant(data))
             predictions.append(pred)
 
-    filename = os.path.basename(os.path.normpath(args.directory)) + '.pkl'
-    with open(filename, 'wb') as f:
+    with open(dest_filename, 'wb') as f:
         pickle.dump(predictions, f)
