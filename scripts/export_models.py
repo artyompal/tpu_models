5e100b0b95133a5685c706c75c467dc55f93f3c5
artyom <artyom@artyom>
2019-09-30 14:32:22 +0100

    added a script for batch models export

---
+++ scripts/export_models.py
@@ -0,0 +1,17 @@
+
+import argparse
+import subprocess
+import yaml
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('models_yaml', help='file with models info, yaml format', type=str)
+    args = parser.parse_args()
+
+    with open(args.models_yaml) as f:
+        models = yaml.load(f, Loader=yaml.SafeLoader)
+
+    for model in models:
+        subprocess.run(['export_saved_model.sh', model['dataset'], model['version'],
+                        str(model['step'])])
