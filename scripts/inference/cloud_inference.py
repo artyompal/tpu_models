b70a9c4599ffe02526ad7adfe1b6dc0c5b7a9301
artyom <artyom@artyom>
2019-10-01 17:58:54 +0100

    changed cloud inference script

--- scripts/inference/cloud_inference.py
+++ scripts/inference/cloud_inference.py
@@ -19,48 +19,68 @@ if __name__ == '__main__':
     placeholder = 'placeholder.csv'
     gs_base_path = 'gs://new_tpu_storage'
 
-    for model in models:
-        model_dir = gs_base_path + '/predicts/' + model["version"] + '-' + model["dataset"] + '-' \
-                  + str(model['resolution'])
-        res = sp.run(['gsutil', 'stat', model_dir + '/' + placeholder])
+    with open('ready_models.txt') as f:
+        ready_models = set(s.rstrip() for s in f)
 
+    for model in models:
+        img_res = str(model['resolution'])
+        model_name = model['version'] + '-' + model['dataset'] + '-' + img_res
+        gs_placeholder = gs_base_path + '/predicts/' + model_name + '_placeholder.csv'
+
+        res = sp.run(['gsutil', 'stat', gs_placeholder])
         if res.returncode == 0:
-            print('\n' + model_dir + ' exists')
+            print('\n' + gs_placeholder + ' already exists')
             continue
 
 
-        print('\n' + model_dir + ' does not exist, starting inference')
+        print('\n' + gs_placeholder + ' does not exist, starting inference')
 
         sp.run(['touch', placeholder], check=True)
-        sp.run(['gsutil', 'cp', placeholder, model_dir + '/'], check=True)
+        sp.run(['gsutil', 'cp', placeholder, gs_placeholder], check=True)
 
-        resolutions = [model['resolution']]
+        gs_model_dir = gs_base_path + '/final/' + model_name
 
-        for img_res in resolutions:
-            model_name = model["version"] + '-' + model["dataset"] + '-' + str(img_res)
-            gs_model_dir = gs_base_path + '/final/' + model_name
+        local_model_dir = 'models/' + model_name
+        dst_predict_path = 'predictions/' + model_name + '.pkl'
+        gs_predict_path = gs_base_path + '/predicts/' + model_name + '.pkl'
 
-            dst_model_dir = 'models/' + model_name
 
-            if not os.path.exists(dst_model_dir):
-                os.makedirs(dst_model_dir)
-                sp.run(['gsutil', '-m', 'cp', '-r', gs_model_dir + '/*', dst_model_dir],
-                       check=True)
+        # check whether we have this model locally
+        if model_name in ready_models:
+            print(model_name + ' already exists locally')
+            continue
 
-            os.makedirs('predictions', exist_ok=True)
-            dst_predict = 'predictions/' + model_name + '.pkl'
+        # check whether we have this model in the cloud
+        res = sp.run(['gsutil', 'stat', gs_predict_path])
+        if res.returncode == 0:
+            print(gs_predict_path + ' already exists in the cloud')
+            continue
 
-            CHECK = False
-            if socket.gethostname() in ['cppg', 'devbox']:
-                sp.run(['./docker_run.sh', 'python', 'inference.py', dst_model_dir,
-                        '--destination', dst_predict], check=CHECK)
-            else:
-                sp.run([sys.executable, 'inference.py', dst_model_dir, '--destination', dst_predict],
-                       check=CHECK)
 
-            if not os.path.exists(dst_predict):
-                dst_predict = '../' + dst_predict
+        if not os.path.exists(local_model_dir):
+            os.makedirs(local_model_dir)
+            sp.run(['gsutil', '-m', 'cp', '-r', gs_model_dir + '/*', local_model_dir],
+                   check=True)
 
-            if not args.suppress_upload:
-                print('prediction finished, uploading the model')
-                sp.run(['gsutil', '-m', 'cp', '-r', dst_predict, gs_base_path + '/predicts/'])
+        os.makedirs('predictions', exist_ok=True)
+
+
+        CHECK = False
+
+        if socket.gethostname() in ['cppg', 'devbox']:
+            res = sp.run(['./docker_run.sh', 'python', 'inference.py', local_model_dir,
+                          '--destination', dst_predict_path], check=CHECK)
+        else:
+            res = sp.run([sys.executable, 'inference.py', local_model_dir, '--destination',
+                          dst_predict_path], check=CHECK)
+
+        if res.returncode != 0:
+            sp.run(['gsutil', 'rm', gs_placeholder])
+            continue
+
+        if not os.path.exists(dst_predict_path):
+            dst_predict_path = '../' + dst_predict_path
+
+        if not args.suppress_upload:
+            print('prediction finished, uploading the model')
+            sp.run(['gsutil', 'cp', dst_predict_path, gs_predict_path])
